0c9165a9ad2cd8c95e23f478e9272e327b9d205a
Rashmidhakne <48613787+Rashmidhakne@users.noreply.github.com>
2019-03-16 12:34:09 +0530

    updateprime.java
    exclamatory sign

--- prime.java
+++ prime.java
@@ -15,7 +15,7 @@ public class Prime {
         }
 
         if (!flag)
-            System.out.println(num + " is a prime number.");
+            System.out.println(num + " is a prime number.!!!!");
         else
             System.out.println(num + " is not a prime number.");
     }
